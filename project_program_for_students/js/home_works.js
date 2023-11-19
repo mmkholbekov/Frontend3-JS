d26b01ea4c5f9fc8fd3275c419d30b7255773580
mmkholbekov <120006110+mmkholbekov@users.noreply.github.com>
2023-11-19 22:27:43 +0400

    Hw2

--- project_program_for_students/js/home_works.js
+++ project_program_for_students/js/home_works.js
@@ -11,10 +11,10 @@ gmailButton.addEventListener('click', () => {
     const isValid = isValidGmail(email);
 
     if (isValid) {
-        gmailResult.textContent = 'Правильно';
-        gmailResult.style.color = 'green';
+        gmailResult.textContent = 'Правильно!';
+        gmailResult.style.color = '#00FA9A';
     } else {
-        gmailResult.textContent = 'Неправильно';
+        gmailResult.textContent = 'Неправильно! Повторите';
         gmailResult.style.color = 'red';
     }
 });
@@ -22,3 +22,94 @@ gmailButton.addEventListener('click', () => {
 function isValidGmail(email) {
     return gmailRegex.test(email);
 }
+
+// MOVE BLOCK
+
+const childBlock = document.querySelector('.child_block')
+
+let positionX = 0
+let positionY = 0
+
+const moveChildBlock = () => {
+    if (positionX < 448 && positionY === 0) {
+        positionX++
+        childBlock.style.left = `${positionX}px`
+        setTimeout(moveChildBlock, 5)
+    } else if (positionX >= 448 && positionY < 448 ){
+        positionY++
+        childBlock.style.top = `${positionY}px`
+        setTimeout(moveChildBlock, 5)
+    } else if (positionX > 0 && positionY > 0) {
+        positionX--
+        childBlock.style.left = `${positionX}px`
+        setTimeout(moveChildBlock, 5)
+    } else if (positionX === 0 && positionY > 0) {
+        positionY--
+        setTimeout(moveChildBlock, 5)
+        childBlock.style.top = `${positionY}px`
+    }
+}
+moveChildBlock()
+
+
+// COUNTER APP
+
+document.addEventListener('DOMContentLoaded', function () {
+    let minutes = 0
+    let seconds = 0
+    let mlSeconds = 0
+    let intervalId
+
+    const minutesElement = document.getElementById('minutesS')
+    const secondsElement = document.getElementById('secondsS')
+    const mlSecondsElement = document.getElementById('ml-secondsS')
+    const startButton = document.getElementById('start')
+    const stopButton = document.getElementById('stop')
+    const resetButton = document.getElementById('reset')
+
+    const updateTimer = () => {
+        mlSeconds++
+        if (mlSeconds === 100) {
+            mlSeconds = 0
+            seconds++
+        }
+        if (seconds === 60) {
+            seconds = 0
+            minutes++
+        }
+
+        updateDisplay()
+    }
+
+    const updateDisplay = () => {
+        minutesElement.textContent = formatTime(minutes)
+        secondsElement.textContent = formatTime(seconds)
+        mlSecondsElement.textContent = formatTime(mlSeconds)
+    }
+
+    const formatTime = (time) => {
+        return time < 10 ? `0${time}` : time
+    }
+
+    const startTimer = () => {
+        intervalId = setInterval(updateTimer, 10)
+    }
+
+    const stopTimer = () => {
+        clearInterval(intervalId)
+        intervalId = null
+    }
+
+    const resetTimer = () => {
+        minutes = 0
+        seconds = 0
+        mlSeconds = 0
+        updateDisplay()
+        stopTimer()
+    }
+
+    startButton.addEventListener('click', startTimer)
+    stopButton.addEventListener('click', stopTimer)
+    resetButton.addEventListener('click', resetTimer)
+})
+
